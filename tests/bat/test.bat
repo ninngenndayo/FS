41c8f37f2dde141c5cdc0c0055c5dbe2107f6d3c
ninngenndayo <familynamepersonalname@gmail.com>
2026-02-22 21:11:06 +0900

    test

---
+++ tests/bat/test.bat
@@ -0,0 +1,9 @@
+@echo off
+echo =====================================
+echo   GitHub Actions からこんにちは！
+echo   Windows runner で動いてます
+echo =====================================
+echo Current time: %DATE% %TIME%
+echo User: %USERNAME%
+echo Working dir: %CD%
+pause
